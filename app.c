602246f6266a61751ae7aad9ff86297ff5797bef
Marat <voland.fagot@gmail.com>
2025-06-19 21:50:33 +0300

    Initinal commit

---
+++ app.c
@@ -0,0 +1,13 @@
+//mingw32-make    компиляция
+#include <stdio.h>
+#include <conio.h>
+//#include "utils.h"
+
+
+int main(){
+
+    printf("Hello Git and GitHub!!!");
+
+    getch();
+    return 0;
+}
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef UTiLS_H
+#define UTILS_H
+
+int add(int, int);
+
+#endif
