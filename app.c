acfee87e887bdb0c38e0d312ea7f7cf9d5cc978d
Marat <voland.fagot@gmail.com>
2025-06-24 19:10:03 +0300

    Правка

--- app.c
+++ app.c
@@ -6,7 +6,29 @@
 
 int main(){
 
-    printf("Hello World!!!");
+    int n = 0;
+    int digit_before = 0, digit_after = 0;
+    unsigned char flag = 0;
+
+    scanf("%d", &n);
+    scanf("%d", &digit_before);
+    scanf("%d", &digit_after);
+    --n;
+    while(n != 1){
+        if(digit_before >= digit_after){
+            flag = 1;
+        }
+        digit_before = digit_after;
+        scanf("%d", &digit_after);
+        --n;
+    }
+    
+    if(digit_before >= digit_after){
+            flag = 1;
+        }
+
+    if(flag == 0) printf("True");
+    else printf("False");
 
     getch();
     return 0;
